d290bfc87399faaead05d8e3e0697044017fcfb6
kjshive <kjshive@millersville.edu>
2025-03-15 16:29:48 -0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,23 @@
+var today = new Date();
+var hourNow = today.getHours();
+var minutes = today.getMinutes();
+var period = hourNow >= 12 ? 'PM' : 'AM';
+
+var hour12 = hourNow % 12 || 12; 
+
+var greeting;
+if (hourNow > 18) {
+    greeting = 'Good evening';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'Good morning';
+} else {
+    greeting = 'Welcome';
+}
+
+document.write(greeting + "<br>");
+document.write("<br>");
+
+document.write("The time is " + hour12 + ":");
+document.write(minutes.toString().padStart(2, '0') + " " + period);
